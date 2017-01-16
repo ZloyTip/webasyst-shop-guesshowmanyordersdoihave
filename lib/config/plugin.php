0de0a9673dd9ed9cd41796e45f3c794919baa7b2
Eugen <jk@47.kiev.ua>
2017-01-16 10:53:55 +0200

    initial commit

---
+++ lib/config/plugin.php
@@ -0,0 +1,11 @@
+<?php
+return array (
+  'name' => 'Guesshowmanyordersdoihave',
+  'icon' => 'img/guesshowmanyordersdoihave.gif',
+  'version' => '1.0.0',
+  'vendor' => '972539',
+  'handlers' => 
+  array (
+      'order_action.create' => 'orderActionCreate'
+  ),
+);
---
+++ lib/shopGuesshowmanyordersdoihavePlugin.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class shopGuesshowmanyordersdoihavePlugin extends shopPlugin
+{
+
+    public function orderActionCreate($data)
+    {
+        $m = new waModel();
+        $max_order_id = $m->query('SELECT MAX(id) FROM shop_order')->fetchField();
+        $new_order_id = (int) ($max_order_id+mt_rand(1,20));
+        $m->query('ALTER TABLE shop_order AUTO_INCREMENT = i:order_id', array('order_id' => $new_order_id));
+    }
+}
